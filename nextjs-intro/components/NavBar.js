ca3a140d19acf6a5fc8d3b0fee7a9dbb7c01320a
gitchan <eunkeeeee@gmail.com>
2023-12-14 21:27:21 +0900

    feat: styles JSX 적용

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
@@ -8,21 +7,22 @@ export default function NavBar() {
     return (
         <nav>
             <Link href="/" legacyBehavior>
-                <a className={
-                    [
-                        styles.link,
-                        router.pathname === "/" ? styles.active : ""
-                    ].join(" ")
-                }>Home</a>
+                <a>Home</a>
             </Link>
             <Link href="/about" legacyBehavior>
-                <a className={
-                    [
-                        styles.link,
-                        router.pathname === "/about" ? styles.active : ""
-                    ].join(" ")
-                }> About < /a>
+                <a>About</a>
             </Link>
+            <style jsx>{
+                `
+                  nav {
+                    background-color: tomato;
+                  }
+
+                  a {
+                    text-decoration: none;
+                  }
+                `
+            }</style>
         </nav>
     );
 }
