ced73c06010f952064b7b7a121e9ba728a13ae8c
gitchan <eunkeeeee@gmail.com>
2023-12-14 20:59:23 +0900

    feat: a 태그에 style 적용

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function NavBar() {
+    const router = useRouter();
+
     return (
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+            <Link href="/" legacyBehavior>
+                <a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a>
+            </Link>
+            <Link href="/about" legacyBehavior>
+                <a style={{color: router.pathname === "/about" ? "red" : "blue"}}> About < /a>
+            </Link>
         </nav>
     );
-};
+}
