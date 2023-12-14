b2ef1b93d6395fe2f6ff1ca621792d47fee045cb
gitchan <eunkeeeee@gmail.com>
2023-12-14 21:17:27 +0900

    feat: 1개의 컴포넌트에 2개의 className 설정하기 - 백틱으로 문자열 합치기

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,17 +1,23 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
-import styles from "./NavBar.module.css"
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     const router = useRouter();
 
     return (
-        <nav className={styles.nav}>
+        <nav>
             <Link href="/" legacyBehavior>
-                <a>Home</a>
+                <a className={
+                    `${styles.link} ${
+                        router.pathname === "/" ? styles.active : ""
+                    }`
+                }>Home</a>
             </Link>
             <Link href="/about" legacyBehavior>
-                <a> About < /a>
+                <a className={`${styles.link} ${
+                    router.pathname === "/about" ? styles.active : ""
+                }`}> About < /a>
             </Link>
         </nav>
     );
