dd801a26df7c0d5d5007c14ff7037ceca65b62e1
gitchan <eunkeeeee@gmail.com>
2023-12-15 12:19:14 +0900

    feat: Custom _app.js 생성

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -7,10 +7,10 @@ export default function NavBar() {
     return (
         <nav>
             <Link href="/" legacyBehavior>
-                <a>Home</a>
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a>
             </Link>
             <Link href="/about" legacyBehavior>
-                <a>About</a>
+                <a className={router.pathname === "/about" ? "active" : ""}>About</a>
             </Link>
             <style jsx>{
                 `
@@ -20,6 +20,10 @@ export default function NavBar() {
 
                   a {
                     text-decoration: none;
+                  }
+
+                  .active {
+                    color: yellow;
                   }
                 `
             }</style>
