b41391fb2f64c9445f2f49635a591538dfca028d
gitchan <eunkeeeee@gmail.com>
2023-12-21 17:50:51 +0900

    refactor: SEO 컴포넌트 분리

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
+import Seo from "@/components/Seo";
 
 export default function About() {
     return (
         <div>
-            <Head>
-                <title>About | Gitchan Movies</title>
-            </Head>
+            <Seo title="About"/>
             <h1>About</h1>
         </div>
     );
