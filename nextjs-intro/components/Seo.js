98aa54f88d20767f5b028f66b249b61d13bee7d4
깃짱 <107979804+gitchannn@users.noreply.github.com>
2023-12-21 18:00:04 +0900

    feat: Head Title 메타데이터 설정 ("next/head") (#4)
    * feat: 페이지 Head 설정 (브라우저 표기)
    
    * refactor: SEO 컴포넌트 분리

---
+++ nextjs-intro/components/Seo.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Seo({title}) {
+    return (
+        <Head>
+            <title>{title} | Gitchan Movies</title>
+        </Head>
+    )
+
+};
