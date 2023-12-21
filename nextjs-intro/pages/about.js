f735858b345aff9e6b578df85b98f3b6df03a342
gitchan <eunkeeeee@gmail.com>
2023-12-21 17:48:44 +0900

    feat: 페이지 Head 설정 (브라우저 표기)

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function About() {
     return (
         <div>
+            <Head>
+                <title>About | Gitchan Movies</title>
+            </Head>
             <h1>About</h1>
         </div>
     );
