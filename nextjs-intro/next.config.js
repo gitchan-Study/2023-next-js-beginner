4a3f5b0b7c59d595d5db1eef2160f1acd4c2bb71
gitchan <eunkeeeee@gmail.com>
2024-01-02 15:16:54 +0900

    feat: Movie API 연동

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const {API_KEY} = process.env;
 
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/blog",
+                destination: "https://engineerinsight.tistory.com",
+                permanent: false
+            }
+        ]
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/movies",
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+            }
+        ]
+    },
+}
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,29 @@
 import Seo from "@/components/Seo";
+import {useEffect, useState} from "react";
+
 
 export default function Home() {
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+        (async () => {
+            const {results} = await (
+                await fetch(
+                    `/api/movies`
+                )
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
+
     return (
         <div>
             <Seo title="Home"/>
-            <h1>Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     );
 }
